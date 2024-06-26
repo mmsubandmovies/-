73c201670a248fff8630b796f041a2d16f67823f
mmsubandmovies <155901935+mmsubandmovies@users.noreply.github.com>
2024-06-26 19:51:57 +0630

    Create prosport.js

---
+++ prosport.js
@@ -0,0 +1,78 @@
+function truepresport1() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://gmlive.ddns.net:5000/live/kun5g/kun5g2024/129.m3u8#Intent;scheme=http;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=NGTV;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function truepresport2() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://gmlive.ddns.net:5000/live/kun5g/kun5g2024/130.m3u8#Intent;scheme=http;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=NGTV;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function truepresport3() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://gmlive.ddns.net:5000/live/kun5g/kun5g2024/131.m3u8#Intent;scheme=http;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=NGTV;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function truepresport4() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://gmlive.ddns.net:5000/live/kun5g/kun5g2024/132.m3u8#Intent;scheme=http;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=NGTV;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function truepresport5() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://gmlive.ddns.net:5000/live/kun5g/kun5g2024/1.m3u8#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=NGTV;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function truesport1() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://gmlive.ddns.net:5000/live/kun5g/kun5g2024/96.m3u8#Intent;scheme=http;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=NGTV;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function truesport2() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://gmlive.ddns.net:5000/live/kun5g/kun5g2024/97.m3u8#Intent;scheme=http;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=NGTV;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function truesport3() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://gmlive.ddns.net:5000/live/kun5g/kun5g2024/98.m3u8#Intent;scheme=http;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=NGTV;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function truesport5() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://gmlive.ddns.net:5000/live/kun5g/kun5g2024/99.m3u8#Intent;scheme=http;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=NGTV;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function truesport7() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://gmlive.ddns.net:5000/live/kun5g/kun5g2024/100.m3u8#Intent;scheme=http;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=NGTV;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function pptvhd() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://ubn-streamer1.cdn.3bbtv.com:8443/3bb/live/36/36.mpd#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=NGTV;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function mcothd() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://ubn-streamer1.cdn.3bbtv.com:8443/3bb/live/30/30.mpd#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=NGTV;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function ไทยรัฐTV32() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://ubn-streamer1.cdn.3bbtv.com:8443/3bb/live/32/32.mpd#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=NGTV;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function cnc() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://gmlive.ddns.net:5000/live/kun5g/kun5g2024/5.m3u8#Intent;scheme=http;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=NGTV;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function mytv() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://gmlive.ddns.net:5000/live/kun5g/kun5g2024/6.m3u8#Intent;scheme=http;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=NGTV;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function cctv5() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://gmlive.ddns.net:5000/live/kun5g/kun5g2024/72.m3u8#Intent;scheme=http;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=NGTV;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function cctvplus5() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://gmlive.ddns.net:5000/live/kun5g/kun5g2024/73.m3u8#Intent;scheme=http;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=NGTV;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function sonysport1() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://gmlive.ddns.net:5000/live/kun5g/kun5g2024/122.m3u8#Intent;scheme=http;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=NGTV;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function sonysport2() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://gmlive.ddns.net:5000/live/kun5g/kun5g2024/123.m3u8#Intent;scheme=http;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=NGTV;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function sonysport3() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://gmlive.ddns.net:5000/live/kun5g/kun5g2024/124.m3u8#Intent;scheme=http;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=NGTV;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function sonysport4() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://gmlive.ddns.net:5000/live/kun5g/kun5g2024/125.m3u8#Intent;scheme=http;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=NGTV;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function sonysport5() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://gmlive.ddns.net:5000/live/kun5g/kun5g2024/126.m3u8#Intent;scheme=http;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=NGTV;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function s_premier() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://gmlive.ddns.net:5000/live/kun5g/kun5g2024/95.m3u8#Intent;scheme=http;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=NGTV;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function s_football() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://gmlive.ddns.net:5000/live/kun5g/kun5g2024/106.m3u8#Intent;scheme=http;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=NGTV;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function s_laliga() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://gmlive.ddns.net:5000/live/kun5g/kun5g2024/105.m3u8#Intent;scheme=http;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=NGTV;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function s_grandstand() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://gmlive.ddns.net:5000/live/kun5g/kun5g2024/93.m3u8#Intent;scheme=http;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=NGTV;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
